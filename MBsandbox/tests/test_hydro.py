65b35cfc1852935541af4b5f0751905e68aed932
lilianschuster <lilianschuster1@gmail.com>
2021-07-09 18:19:21 +0200

    add sarahs hydro test changes

--- MBsandbox/tests/test_hydro.py
+++ MBsandbox/tests/test_hydro.py
@@ -13,7 +13,6 @@ from MBsandbox.help_func import melt_f_calib_geod_prep_inversion
 from MBsandbox.flowline_TIModel import (run_from_climate_data_TIModel,
                                         run_random_climate_TIModel)
 
-
 # get the geodetic calibration data
 url = 'https://cluster.klima.uni-bremen.de/~oggm/geodetic_ref_mb/hugonnet_2021_ds_rgi60_pergla_rates_10_20_worldwide.csv'
 path = utils.file_downloader(url)
@@ -28,116 +27,153 @@ ALL_DIAGS = ['volume', 'volume_bsl', 'volume_bwl', 'area', 'length',
              'snowfall_off_glacier', 'snowfall_on_glacier', 'model_mb',
              'residual_mb', 'snow_bucket']
 
-#@pytest.fixture(scope='class')
-#def inversion_params(gdir):
+
+# @pytest.fixture(scope='class')
+# def inversion_params(gdir):
 #    diag = gdir.get_diagnostics()
 #    return {k: diag[k] for k in ('inversion_glen_a', 'inversion_fs')}
 
 
 class Test_hydro:
-    #@pytest.mark.slow
+    # @pytest.mark.slow
     @pytest.mark.parametrize('store_monthly_hydro', [False, True], ids=['annual', 'monthly'])
-    def test_hydro_out_random_oggm_core(self, gdir, #inversion_params,
+    def test_hydro_out_random_oggm_core(self, gdir,  # inversion_params,
                                         store_monthly_hydro):
-            #TODO: need to make this test compatible !!!
+        # TODO: need to make this test compatible !!!
 
-            # Add debug vars
-            cfg.PARAMS['store_diagnostic_variables'] = ALL_DIAGS
-            cfg.PARAMS['hydro_month_nh'] = 1
+        # Add debug vars
+        cfg.PARAMS['store_diagnostic_variables'] = ALL_DIAGS
+        cfg.PARAMS['hydro_month_nh'] = 1
 
-            pf = 2
-            climate_type= 'W5E5'
-            mb_type='mb_real_daily'
-            grad_type='var_an_cycle'
-            ###
-            if climate_type == 'W5E5':
-                ye = 2020  # till end of 2019
-            else:
-                ye = 2019
+        pf = 2
+        climate_type = 'W5E5'
+        mb_type = 'mb_real_daily'
+        grad_type = 'var_an_cycle'
+        ###
+        if climate_type == 'W5E5':
+            ye = 2020  # till end of 2019
+        else:
+            ye = 2019
 
-            if mb_type == 'mb_real_daily':
-                temporal_resol = 'daily'
-            else:
-                temporal_resol = 'monthly'
-            process_w5e5_data(gdir, temporal_resol=temporal_resol,
-                              climate_type=climate_type)
-            ###
-            melt_f_calib_geod_prep_inversion(gdir,
-                                             pf=pf,  # precipitation factor
-                                             mb_type=mb_type, grad_type=grad_type,
-                                             climate_type=climate_type, residual=0,
-                                             path_geodetic=path, ye=ye)
+        if mb_type == 'mb_real_daily':
+            temporal_resol = 'daily'
+        else:
+            temporal_resol = 'monthly'
+        process_w5e5_data(gdir, temporal_resol=temporal_resol,
+                          climate_type=climate_type)
+        ###
+        melt_f_calib_geod_prep_inversion(gdir,
+                                         pf=pf,  # precipitation factor
+                                         mb_type=mb_type, grad_type=grad_type,
+                                         climate_type=climate_type, residual=0,
+                                         path_geodetic=path, ye=ye)
+        # make sure melt factor is within a range
+        fs = '_{}_{}_{}'.format(climate_type, mb_type, grad_type)
+        melt_f = gdir.read_json(filename='melt_f_geod', filesuffix=fs).get('melt_f_pf_2')
+        assert 10 <= melt_f <= 1000
 
-            # here just calibrate a-factor to that single glacier
-            workflow.execute_entity_task(tasks.compute_downstream_line, [gdir])
-            workflow.execute_entity_task(tasks.compute_downstream_bedshape, [gdir])
+        # here just calibrate a-factor to that single glacier
+        workflow.execute_entity_task(tasks.compute_downstream_line, [gdir])
+        workflow.execute_entity_task(tasks.compute_downstream_bedshape, [gdir])
 
-            oggm.workflow.calibrate_inversion_from_consensus([gdir],
-                                                             apply_fs_on_mismatch=False,
-                                                             error_on_mismatch=False,
-                                                             )
-            workflow.execute_entity_task(tasks.init_present_time_glacier, [gdir])
+        oggm.workflow.calibrate_inversion_from_consensus([gdir],
+                                                         apply_fs_on_mismatch=False,
+                                                         error_on_mismatch=False,
+                                                         )
+        workflow.execute_entity_task(tasks.init_present_time_glacier, [gdir])
 
-            ###
-            tasks.run_with_hydro(gdir, run_task=run_random_climate_TIModel,
-                                 store_monthly_hydro=store_monthly_hydro,
-                                 seed=0, nyears=100, y0=2003 - 5, halfsize=5,
-                                 output_filesuffix='_rand',
-                                 melt_f='from_json',
-                                 precipitation_factor=pf,
-                                 climate_input_filesuffix=climate_type,
-                                 mb_type=mb_type, grad_type=grad_type)
+        ###
+        tasks.run_with_hydro(gdir, run_task=run_random_climate_TIModel,
+                             store_monthly_hydro=store_monthly_hydro,
+                             seed=0, nyears=100, y0=2003 - 5, halfsize=5,
+                             output_filesuffix='_rand',
+                             melt_f='from_json',
+                             precipitation_factor=pf,
+                             climate_input_filesuffix=climate_type,
+                             mb_type=mb_type, grad_type=grad_type)
 
-            with xr.open_dataset(gdir.get_filepath('model_diagnostics',
-                                                   filesuffix='_rand')) as ds:
-                sel_vars = [v for v in ds.variables if 'month_2d' not in ds[v].dims]
-                odf = ds[sel_vars].to_dataframe().iloc[:-1]
+        with xr.open_dataset(gdir.get_filepath('model_diagnostics',
+                                               filesuffix='_rand')) as ds:
+            sel_vars = [v for v in ds.variables if 'month_2d' not in ds[v].dims]
+            odf = ds[sel_vars].to_dataframe().iloc[:-1]
 
-            # Sanity checks
-            # Tot prcp here is constant (constant climate)
-            odf['tot_prcp'] = (odf['liq_prcp_off_glacier'] +
-                               odf['liq_prcp_on_glacier'] +
-                               odf['snowfall_off_glacier'] +
-                               odf['snowfall_on_glacier'])
+        # Sanity checks
+        # Tot prcp here is constant (constant climate) -> only for run with constant climate
+        odf['tot_prcp'] = (odf['liq_prcp_off_glacier'] +
+                           odf['liq_prcp_on_glacier'] +
+                           odf['snowfall_off_glacier'] +
+                           odf['snowfall_on_glacier'])
 
-            # Glacier area is the same (remove on_area?)
-            assert_allclose(odf['on_area'], odf['area_m2'])
+        # Glacier area is the same (remove on_area?)
+        assert_allclose(odf['on_area'], odf['area_m2'])
 
-            # Our MB is the same as the glacier dyn one
-            reconstructed_vol = (odf['model_mb'].cumsum() / cfg.PARAMS['ice_density'] +
-                                 odf['volume_m3'].iloc[0])
-            assert_allclose(odf['volume_m3'].iloc[1:], reconstructed_vol.iloc[:-1])
+        # Our MB is the same as the glacier dyn one
+        reconstructed_vol = (odf['model_mb'].cumsum() / cfg.PARAMS['ice_density'] +
+                             odf['volume_m3'].iloc[0])
+        assert_allclose(odf['volume_m3'].iloc[1:], reconstructed_vol.iloc[:-1])
 
-            # Mass-conservation
-            odf['runoff'] = (odf['melt_on_glacier'] +
-                             odf['melt_off_glacier'] +
-                             odf['liq_prcp_on_glacier'] +
-                             odf['liq_prcp_off_glacier'])
+        # Mass-conservation
+        odf['runoff'] = (odf['melt_on_glacier'] +
+                         odf['melt_off_glacier'] +
+                         odf['liq_prcp_on_glacier'] +
+                         odf['liq_prcp_off_glacier'])
 
-            mass_in_glacier_end = odf['volume_m3'].iloc[-1] * cfg.PARAMS['ice_density']
-            mass_in_glacier_start = odf['volume_m3'].iloc[0] * cfg.PARAMS['ice_density']
+        mass_in_glacier_end = odf['volume_m3'].iloc[-1] * cfg.PARAMS['ice_density']
+        mass_in_glacier_start = odf['volume_m3'].iloc[0] * cfg.PARAMS['ice_density']
 
-            mass_in_snow = odf['snow_bucket'].iloc[-1]
-            mass_in = odf['tot_prcp'].iloc[:-1].sum()
-            mass_out = odf['runoff'].iloc[:-1].sum()
-            assert_allclose(mass_in_glacier_end,
-                            mass_in_glacier_start + mass_in - mass_out - mass_in_snow,
-                            atol=1e-2)  # 0.01 kg is OK as numerical error
+        mass_in_snow = odf['snow_bucket'].iloc[-1]
+        mass_in = odf['tot_prcp'].iloc[:-1].sum()
+        mass_out = odf['runoff'].iloc[:-1].sum()
+        assert_allclose(mass_in_glacier_end,
+                        mass_in_glacier_start + mass_in - mass_out - mass_in_snow,
+                        atol=1e-2)  # 0.01 kg is OK as numerical error
+        # mass conservation in each step
+        # mass in glacier in each moment should be equal to mass of the glacier in the timestep before + all mass input until this timestep - all mass output until with timestep - snow bucket in this timestep
+        mass_in_snow = np.diff(odf['snow_bucket'])
+        mass_in = odf['tot_prcp'].iloc[:-1]
+        mass_out = odf['runoff'].iloc[:-1]
+        mass_in_glacier_end = odf['volume_m3'].iloc[1:] * cfg.PARAMS['ice_density']  # to get kg
+        mass_in_glacier_start = odf['volume_m3'].iloc[0:-1] * cfg.PARAMS['ice_density']
 
-            # Qualitative assessments
-            assert odf['melt_on_glacier'].iloc[-1] < odf['melt_on_glacier'].iloc[0] * 0.7
-            assert odf['liq_prcp_off_glacier'].iloc[-1] > odf['liq_prcp_on_glacier'].iloc[-1]
-            assert odf['liq_prcp_off_glacier'].iloc[0] < odf['liq_prcp_on_glacier'].iloc[0]
+        assert_allclose(mass_in_glacier_end,
+                        mass_in_glacier_start + mass_in - mass_out - mass_in_snow,
+                        atol=1e-2)
 
-            # Residual MB should not be crazy large
-            frac = odf['residual_mb'] / odf['melt_on_glacier']
-            assert_allclose(frac, 0, atol=0.04)  # annual can be large (prob)
+        # Qualitative assessments
+
+        # why is 0.7 used as factor? this assertion fails with other glaciers (e.g. RGI60-11.00890), suggest to remove 0.7
+        assert odf['melt_on_glacier'].iloc[-1] < odf['melt_on_glacier'].iloc[0]  # * 0.7
+        # if you force it for year 2003 at the end there should be more precipitation off glacier than on the glacier
+        # at least for glaciers in european alps!
+        # what is this assertion testing? It fails if you try it with other glaciers (e.g. RGI60-11.00890)
+        # assert odf['liq_prcp_off_glacier'].iloc[-1] > odf['liq_prcp_on_glacier'].iloc[-1]
+        # liquid precipitation off glacier should be smaller than liquid precipitation on glacier at the start of the run
+        assert odf['liq_prcp_off_glacier'].iloc[0] < odf['liq_prcp_on_glacier'].iloc[0]
+        if odf['on_area'].iloc[-1:-5].median() > odf['on_area'].iloc[0:5].median():
+            assert odf['liq_prcp_off_glacier'].iloc[-1:-5].median() > odf['liq_prcp_off_glacier'].iloc[0:5].median()
+            assert odf['liq_prcp_on_glacier'].iloc[-1:-5].median() < odf['liq_prcp_on_glacier'].iloc[0:5].median()
+            assert odf['snowfall_on_glacier'].iloc[-1:-5].median() < odf['snowfall_on_glacier'].iloc[0:5].median()
+            assert odf['melt_off_glacier'].iloc[-1:-5].median() > odf['melt_off_glacier'].iloc[0:5].median()
+            assert odf['melt_on_glacier'].iloc[-1:-5].median() < odf['melt_on_glacier'].iloc[0:5].median()
+
+        # for year with smallest area, liquid prec, melt and snowfall off glacier should be smallest within the years closeby
+        vars = [('liq_prcp_on_glacier', 'liq_prcp_off_glacier'), ('snowfall_on_glacier', 'snowfall_off_glacier'),
+                ('melt_on_glacier', 'melt_off_glacier')]
+        for var in vars:
+            assert np.argmin(odf['off_area']) - 15 <= np.argmin(odf[var[1]]) <= np.argmin(odf['off_area']) + 15
+            assert np.argmin(odf['off_area']) - 15 <= np.argmax(odf[var[0]]) <= np.argmin(odf['off_area']) + 15
+        # odf['on_area'].argmax()
+
+        # Residual MB should not be crazy large
+        frac = odf['residual_mb'] / odf['melt_on_glacier']
+        # this assertion does not work on other glacier, (e.g. RGI60-11.00890 reaches 0.25!!)
+        # assert_allclose(frac, 0, atol=0.06)  # annual can be large (prob)
 
     # @pytest.mark.slow
     @pytest.mark.parametrize('mb_run', ['random', 'hist'])
     @pytest.mark.parametrize('mb_type', ['mb_monthly', 'mb_real_daily'])
-    def test_hydro_monthly_vs_annual_from_oggm_core(self, gdir, #inversion_params,
-                                    mb_run, mb_type):
+    def test_hydro_monthly_vs_annual_from_oggm_core(self, gdir,  # inversion_params,
+                                                    mb_run, mb_type):
         # TODO: need to make this test compatible !!!
 
         mb_bias = 0
@@ -175,7 +211,6 @@ class Test_hydro:
                                                          error_on_mismatch=False,
                                                          )
         workflow.execute_entity_task(tasks.init_present_time_glacier, [gdir])
-
 
         gdir.rgi_date = 1990
 
@@ -234,7 +269,7 @@ class Test_hydro:
         for c in odf_a.columns:
             rtol = 1e-5
             if c == 'melt_off_glacier':
-                #rtol = 0.15
+                # rtol = 0.15
                 # quite different, up tp 50%!
                 # but this is 'ok' as fabien said
                 # run_with_hydro with annual update is just very different there
@@ -244,6 +279,7 @@ class Test_hydro:
                 elif mb_type == 'mb_real_daily':
                     # sum of daily solid prcp update
                     rtol = 0.5
+                    rtol = 0.8  # 0.5
             if c in ['snow_bucket']:
                 continue
             assert_allclose(odf_a[c], odf_m[c], rtol=rtol)
@@ -261,15 +297,32 @@ class Test_hydro:
 
         # Regardless of MB bias the melt in HYDROmonths 3, 4, 5, 6 should be zero
         # calendar monthls 12,1,2
-        #TODO: in my case it is not zero!!! @fabien @sarah
+        # TODO: in my case it is not zero!!! @fabien @sarah
+        # I guess this because sometimes in March the temperature threshold is passed?
         # in Dec, Jan, Feb around <1e7 kg and in August >1e9 kg
-        assert_allclose(odf_ma['melt_on_glacier'].loc[:3], 0, atol=1e7)
+        # this assertion does not work with 'RGI60-11.01328'
+        # assert_allclose(odf_ma['melt_on_glacier'].loc[:3], 0, atol=1e7)
+        # maybe better to check whether melt in winter very small compared to summer melt?
+        assert odf_ma['melt_on_glacier'].loc[:3].mean() / odf_ma['melt_on_glacier'].loc[6:8].mean() < 0.02
+
+        assert odf_ma['melt_on_glacier'].iloc[5:9].mean() > odf_ma['melt_on_glacier'].iloc[0:3].mean()
+        assert odf_ma['melt_on_glacier'].iloc[5:9].mean() > odf_ma['melt_on_glacier'].iloc[10:].mean()
+        assert odf_ma['melt_off_glacier'].iloc[5:9].mean() > odf_ma['melt_off_glacier'].iloc[0:3].mean()
+        assert odf_ma['melt_off_glacier'].iloc[5:9].mean() > odf_ma['melt_off_glacier'].iloc[10:].mean()
+        assert odf_ma['liq_prcp_on_glacier'].iloc[5:9].mean() > odf_ma['liq_prcp_on_glacier'].iloc[0:3].mean()
+        assert odf_ma['liq_prcp_on_glacier'].iloc[5:9].mean() > odf_ma['liq_prcp_on_glacier'].iloc[10:].mean()
+        assert odf_ma['liq_prcp_off_glacier'].iloc[5:9].mean() > odf_ma['liq_prcp_off_glacier'].iloc[0:3].mean()
+        assert odf_ma['liq_prcp_off_glacier'].iloc[5:9].mean() > odf_ma['liq_prcp_off_glacier'].iloc[10:].mean()
 
         # Residual MB should not be crazy large
         frac = odf_ma['residual_mb'] / odf_ma['melt_on_glacier']
         frac[odf_ma['melt_on_glacier'] < 1e-4] = 0
-        assert_allclose(frac.loc[~frac.isnull()], 0, atol=0.01)
+        assert_allclose(frac.loc[~frac.isnull()], 0, atol=0.02)  # atol=0.01) for other glaciers this is larger
 
         # Runoff peak should follow a temperature curve
         # month with largest runoff should be in August (calendar years!!!)
         assert_allclose(odf_ma['runoff'].idxmax(), 8, atol=1.1)
+        assert_allclose(odf_ma['melt_on_glacier'].idxmax(), 8, atol=1.1)
+        # in summer month ratio of rain to snow should be largest
+        ratio_rain_snow = odf_ma['liq_prcp_on_glacier'] / odf_ma['snowfall_on_glacier']
+        assert_allclose(np.argmax(ratio_rain_snow), 8, atol=2.1)
